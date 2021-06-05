4bb45911bf029f6ed636b7f459c4592b272a5765
Roman Borisov <riborisov@sbercloud.ru>
2021-06-05 20:17:27 +0300

    refactor test for product card

--- pages/card_page.py
+++ pages/card_page.py
@@ -1,10 +1,29 @@
 from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+from helper import assert_element
 
 
-class CardPage:
+class CardPage(BasePage):
+    PATH = 'index.php?route=product/product&path=57&product_id=49'
     LOGO = (By.CSS_SELECTOR, '#logo')
     QUANTITY = (By.CSS_SELECTOR, '#input-quantity')
     ADD_TO_CART = (By.CSS_SELECTOR, '#button-cart')
     THUMBNAILS = (By.CSS_SELECTOR, '.col-sm-8 .thumbnails')
     HEADER = (By.CSS_SELECTOR, '#top')
     FOOTER = (By.CSS_SELECTOR, 'footer .container')
+    SUCCESS_MSG = (By.CSS_SELECTOR, '.alert.alert-success.alert-dismissible')
+
+    def check_elements_exist(self):
+        browser = self.browser
+        browser.open(self.PATH)
+        browser.find_element(*CardPage.LOGO)
+        browser.find_element(*CardPage.HEADER)
+        browser.find_element(*CardPage.QUANTITY)
+        browser.find_element(*CardPage.THUMBNAILS)
+        browser.find_element(*CardPage.FOOTER)
+
+    def add_to_cart(self):
+        # добавить товар в корзину и получить сообщение об успешном действии
+        self.browser.find_element(*CardPage.ADD_TO_CART).click()
+        assert_element(self.SUCCESS_MSG, self.browser, timeout=2)
+
--- tests/test_item_card.py
+++ tests/test_item_card.py
@@ -1,18 +1,6 @@
-from helper import assert_element
 from pages.card_page import CardPage
-from selenium.webdriver.common.by import By
-
-URL = 'https://demo.opencart.com/index.php?route=product/product&path=57&product_id=49'
 
 
 def test_product_card(browser):
-    browser.get(URL)
-    browser.find_element(*CardPage.LOGO)
-    browser.find_element(*CardPage.HEADER)
-    browser.find_element(*CardPage.QUANTITY)
-    browser.find_element(*CardPage.THUMBNAILS)
-    browser.find_element(*CardPage.FOOTER)
-
-    # добавить товар в корзину и получить сообщение об успешном действии
-    browser.find_element(*CardPage.ADD_TO_CART).click()
-    assert_element((By.CSS_SELECTOR, '.alert.alert-success.alert-dismissible'), browser, timeout=2)
+    CardPage(browser).check_elements_exist()
+    CardPage(browser).add_to_cart()
