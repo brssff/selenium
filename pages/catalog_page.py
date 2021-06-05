7011ff31d4ce8f3f3d80fd67f0f392eee267bad7
Roman Borisov <riborisov@sbercloud.ru>
2021-06-05 19:49:55 +0300

    refactor tests for catalog page

--- pages/catalog_page.py
+++ pages/catalog_page.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
 
-class CatalogPage:
-    URL = 'https://demo.opencart.com/index.php?route=product/category&path=20'
+class CatalogPage(BasePage):
+    PATH = 'index.php?route=product/category&path=20'
 
     LOGO = (By.CSS_SELECTOR, '#logo')
     CATALOG_COLUMN = (By.CSS_SELECTOR, '#column-left')
@@ -13,3 +14,20 @@ class CatalogPage:
     SORT_BY = (By.CSS_SELECTOR, '#input-sort')
     SHOW_LIMIT = (By.CSS_SELECTOR, '#input-limit')
     FOOTER = (By.CSS_SELECTOR, 'footer .container')
+
+    def check_elements_exist_on_page(self):
+        browser = self.browser
+        browser.open(self.PATH)
+        browser.find_element(*self.LOGO)
+        browser.find_element(*self.CATALOG_COLUMN)
+        browser.find_element(*self.SEARCH_INPUT)
+        browser.find_element(*self.LIST_VIEW)
+        browser.find_element(*self.GRID_VIEW)
+        browser.find_element(*self.SORT_BY)
+        browser.find_element(*self.SHOW_LIMIT)
+        browser.find_element(*self.FOOTER)
+
+    def check_product_thumbs(self):
+        product_thumbs = self.browser.find_elements(*CatalogPage.PRODUCT_THUMB)
+        assert len(product_thumbs) == 12,\
+            f"Thumbs on page: {len(product_thumbs)}, but 12 expected"
--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,18 +1,7 @@
-from helper import assert_element
 from pages.catalog_page import CatalogPage
 
 
 def test_catalog_page(browser):
-    browser.get(CatalogPage.URL)
-    browser.find_element(*CatalogPage.LOGO)
-    browser.find_element(*CatalogPage.CATALOG_COLUMN)
-    browser.find_element(*CatalogPage.SEARCH_INPUT)
-    browser.find_element(*CatalogPage.LIST_VIEW)
-    browser.find_element(*CatalogPage.GRID_VIEW)
-    browser.find_element(*CatalogPage.SORT_BY)
-    browser.find_element(*CatalogPage.SHOW_LIMIT)
-    browser.find_element(*CatalogPage.FOOTER)
-    assert_element(CatalogPage.FOOTER, browser, timeout=2)
 
-    product_thumbs = browser.find_elements(*CatalogPage.PRODUCT_THUMB)
-    assert len(product_thumbs) == 12, f"Thumbs on page: {len(product_thumbs)}, but 12 expected"
+    CatalogPage(browser).check_elements_exist_on_page()
+    CatalogPage(browser).check_product_thumbs()
